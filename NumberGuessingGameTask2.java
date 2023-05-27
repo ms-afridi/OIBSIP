67d39b93cb1c1865b183d336e8ea257021f406d1
Md Shahid Afridi <133534836+ms-afridi@users.noreply.github.com>
2023-05-27 09:15:50 +0530

    Update NumberGuessingGameTask2.java

--- NumberGuessingGameTask2.java
+++ NumberGuessingGameTask2.java
@@ -1,84 +1,62 @@
-import java.util.*;
-public class NumberGuessingGame {
-    static ArrayList<Integer> scoreBoard = new ArrayList<Integer>();
-    public static void main(String[] args) {
-        NumberGuessingGame methodChange = new NumberGuessingGame();
-        methodChange.menu(scoreBoard);
-    }
-    public void menu(ArrayList<Integer> scoreBoard) {
-        NumberGuessingGame methodChange = new NumberGuessingGame();
-        Scanner input = new Scanner(System.in);
-        System.out.println("--------------------");
-        System.out.println("Welcome to the number game");
-        System.out.println("1) Play the Game");
-        System.out.println("2) Score Board");
-        System.out.println("3) Exit the game");
-        System.out.println("--------------------");
-        try {
-            System.out.print("What action would you like to do from the above actions? ");
-            int menuOption = input.nextInt();
-            switch (menuOption) {
-                case 1:
-                    System.out.print("\n"+"What would you like the range of the numbers to be? ");
-                    int numberRange = input.nextInt();
-                    int randomNumber = methodChange.randomNumber(numberRange);
-                    methodChange.guessNumber(randomNumber);
-                    break;
-                case 2:
-                    methodChange.displayScoreBoard();
-                    break;
-                case 3:
-                    System.out.println("\n"+"Thanks for playing the game!");
-                    System.exit(1);
-                    break;
-                default:
-                    throw new InputMismatchException("Invalid number entry.Could you please Try again later");
-            }
-        }catch(InputMismatchException e){
-            System.err.println("\n"+e.getMessage() +"\n");
-            menu(scoreBoard);
-        }
-    }
-    public int randomNumber(int numberRange) {
-        Random random = new Random();
-        int randomNumber = random.nextInt(numberRange) + 1;
-        return randomNumber;
-    }
-    public void guessNumber(int randomNumber) {
-        Scanner input = new Scanner(System.in);
-        int userGuess;
-        int guess = 0;
-        do {
-            System.out.print("Enter your guess number: ");
-            userGuess = input.nextInt();
-            guess++;
-            if (userGuess > randomNumber) {
-                System.out.println("Lower");
-            } else if (userGuess < randomNumber) {
-                System.out.println("Higher");
-            }
-        } while (randomNumber != userGuess);
-        System.out.println(" ");
-        if (guess == 1) {
-            System.out.println("You answered number is right in " + guess + " try!");
-        } else {
-            System.out.println("You answered number is right in " + guess + " tries!");
-        }
-        scoreBoard.add(guess);
-        System.out.println(" ");
+import java.util.Scanner;
+import java.util.Random;
 
-        menu(scoreBoard);
-    }
-    public void displayScoreBoard() {
-        System.out.println("--------------------");
-        System.out.println("Score Board");
-        System.out.println("--------------------");
-        System.out.println("Your fastest games today out of all tries is: " +"\n");
-        Collections.sort(scoreBoard);
-        for (Integer scores : scoreBoard) {
-            System.out.println("Finished the number game in " + scores + " tries");
-        }
-        System.out.println(" ");
-        menu(scoreBoard);
-    }
+class Game {
+
+	public int random_num;
+	public int guess;
+	public int noOfGuesses = 0;
+
+	public void setNoOfGuesses(int noOfGuesses) {
+		this.noOfGuesses = noOfGuesses;
+	}
+
+	public int getNoOfGuesses() {
+		return noOfGuesses;
+	}
+
+	Game() {
+		Random rand = new Random();
+		random_num = rand.nextInt(100);
+
+	}
+
+	void TakeUserInput() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Guess the number between 1-100");
+
+		guess = sc.nextInt();
+	}
+
+	boolean IsCorrectNumber() {
+		noOfGuesses++;
+
+		if (guess == random_num) {
+			System.out.format("Yes you get it right , it was %d \nYou guessed in %d chance", guess, noOfGuesses);
+			return true;
+		} else if (guess < random_num) {
+			System.out.println("This Number is Too Low, try to guess the Bigger Number");
+		} else if (guess > random_num) {
+			System.out.println("This number is Too High try to guess the Lower Number......");
+		}
+		return false;
+	}
+
 }
+
+public class GuessTheNumber {
+
+	public static void main(String[] args) {
+
+		Game g = new Game();
+
+		boolean b = false;
+		while (!b) {
+			g.TakeUserInput();
+			b = g.IsCorrectNumber();
+		}
+
+	}
+
+}
+
